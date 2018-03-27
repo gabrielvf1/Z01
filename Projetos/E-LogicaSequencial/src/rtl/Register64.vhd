5af855318f255dd002124f1aa9031f916b132762
gabrielvf1 <vf_gabriel@yahoo.com.br>
2018-03-27 20:20:47 -0300

    register 64 added

--- Projetos/E-LogicaSequencial/src/rtl/Register64.vhd
+++ Projetos/E-LogicaSequencial/src/rtl/Register64.vhd
@@ -13,3 +13,16 @@ entity Register64 is
 		output: out STD_LOGIC_VECTOR(63 downto 0)
 	);
 end entity;
+architecture arch of Register64 is
+  component Register32 is
+	port(
+		clock:   in STD_LOGIC;
+		input:   in STD_LOGIC_VECTOR(31 downto 0);
+		load:    in STD_LOGIC;
+		output: out STD_LOGIC_VECTOR(31 downto 0)
+	);
+  end component;
+  begin
+  r1: Register32 Port Map (clock,input(31 downto 0),load,output(31 downto 0));
+  r2: Register32 Port Map (clock,input(63 downto 32),load,output(63 downto 32));
+  end architecture;
