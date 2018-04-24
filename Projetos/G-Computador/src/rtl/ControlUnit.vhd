39d6d007f42a812d81eb4bcca651da39397e6684
Samuelgranato <samuelgranato87@gmail.com>
2018-04-24 14:38:15 -0300

    teste control_unity

--- Projetos/G-Computador/src/rtl/ControlUnit.vhd
+++ Projetos/G-Computador/src/rtl/ControlUnit.vhd
@@ -21,7 +21,33 @@ entity ControlUnit is
     );
 end entity;
 
+signal porta_in : STD_LOGIC;
+
 architecture arch of ControlUnit is
 begin
+
+zx <= instruction(15) and instruction(12); 
+nx <= instruction(15) and instruction(11);
+zy <= instruction(15) and instruction(10);
+ny <= instruction(15) and instruction(9);
+f  <= instruction(15) and instruction(8);
+no <= instruction(15) and instruction(7);
+
+muxALUI_A <= not instruction(15);
+muxAM_ALUz	<= instruction(13);
+muxSD_ALUx	<= instruction(14);
+
+LoadA <= instruction(15) or instruction(6);
+loadD <= not instruction(14);
+loadS <= instruction(13);
+loadM <= instruction(3);
+
+loadPC <= (instruction(2) and ng) or (instruction(1) and zr) or (instruction(0) and not(ng) and not(zr)) or (instruction(0) and instruction(1) and instruction(2));
+
+
+--loadPC <= (instruction(2) and ng);
+--loadPC <= instruction(1) and zr;
+--loadPC <= instruction(0) and not(ng) and not(zr);
+--loadPC <= instruction(0) and instruction(1) and instruction(2);
 
 end architecture;
